53a780fd3573da9d1d9b071ec7bc670d6171f513
Veadick Srinivasan <pv67lister@gmail.com>
2026-09-17 23:18:01 +0530

    Build integrated intelligent traffic management application

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Intelligent Traffic Management System");
-        System.out.println("Java 21 is working!");
+        SwingUtilities.invokeLater(() -> {
+            TrafficManagementUI ui = new TrafficManagementUI();
+            ui.setVisible(true);
+        });
     }
 }
